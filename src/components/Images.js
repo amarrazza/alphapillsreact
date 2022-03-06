ccaaffb6112f8fef91f8f1ed5025bc0216255c38
Austin Marrazza <56844892+amarrazza@users.noreply.github.com>
2022-03-05 19:23:07 -0800

    gallery navigation

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { useNavigate }  from "react-router-dom";
 import Image from "./Image";
 import imageData from "../imageData/galleryImages";
 
 const Images = () => {
     let { page } = useParams()
+    let navigate = useNavigate();
     let pages = []
     for(let i = 0; i < imageData.length/2; i++){
         pages.push(i);
+    }
+    const leftArrow = () => {
+        if (page <= 0){
+            navigate('/gallery/0')
+        } else {
+            navigate(`/gallery/${page - 1}`)
+        }
+    }
+
+    const rightArrow = () => {
+        if (page >= pages.length - 1){
+            navigate(`/gallery/${page}`)
+        } else {
+            navigate(`/gallery/${page * 1 + 1}`)
+        }
     }
     return(
         <StyledDiv className="imagesContainer">
@@ -20,11 +37,11 @@ const Images = () => {
                 })}
             </div>
             <div className="pageNav">
-                <p className="arrow">&#8592;</p>
-                {pages.map(page => {
-                    return <p className="pageNum">{page}</p>
+                <p className="arrow" onClick={() => leftArrow()}>&#8592;</p>
+                {pages.map(singlePage => {
+                    return <p key={singlePage} className="pageNum" onClick={() => navigate(`/gallery/${singlePage}`)}>{singlePage}</p>
                 })}
-                <p className="arrow">&#8594;</p>
+                <p className="arrow" onClick={() => rightArrow()}>&#8594;</p>
             </div>
         </StyledDiv>
     )
@@ -50,16 +67,22 @@ const StyledDiv = styled.div`
         display: flex;
         flex-flow: row nowrap;
         justify-content: center;
-        width: 10%;
+        width: 20%;
     }
     .pageNum{
         margin: 2.5%;
+        &:hover{
+            cursor: pointer;
+        }
     }
 
     .arrow{
         display: flex;
         align-items: center;
         margin: 2.5% 5%;
+        &:hover{
+            cursor: pointer;
+        }
     }
 
 `
